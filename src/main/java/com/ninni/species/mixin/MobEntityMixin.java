39f62ba6b28632d0fa20813d69b49d3738565eb6
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-10-27 17:15:56 +0200

    made so you can make entities dismount from roombugs

---
+++ src/main/java/com/ninni/species/mixin/MobEntityMixin.java
@@ -0,0 +1,30 @@
+package com.ninni.species.mixin;
+
+import com.ninni.species.entity.RoombugEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
+    private void updatePassengerDismounting(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (this.getVehicle() instanceof RoombugEntity) {
+            this.stopRiding();
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+}
