b1719f92331927aae86b8fd4c8bc874ace4fa9bf
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-01-02 17:14:28 +0100

    Limpet hiding

--- src/main/java/com/ninni/species/client/model/entity/LimpetEntityModel.java
+++ src/main/java/com/ninni/species/client/model/entity/LimpetEntityModel.java
@@ -5,9 +5,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.entity.model.SinglePartEntityModel;
+import net.minecraft.util.math.MathHelper;
 
 import static net.minecraft.client.render.entity.model.EntityModelPartNames.*;
-import static net.minecraft.util.math.MathHelper.clamp;
 import static net.minecraft.util.math.MathHelper.cos;
 
 @Environment(EnvType.CLIENT)
@@ -134,13 +134,39 @@ public class LimpetEntityModel<T extends LimpetEntity> extends SinglePartEntityM
 
     @Override
     public void setAngles(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-        limbDistance = clamp(limbDistance, -0.45F, 0.45F);
         float pi = (float)Math.PI;
 
         float speed = 1.5f;
         float degree = 1.0f;
 
-        this.rightLeg.pitch = cos(limbAngle * speed * 0.6F) * 1.4F * limbDistance;
-        this.leftLeg.pitch = cos(limbAngle * speed * 0.6F + pi) * 1.4F * limbDistance;
+        //eye looking direction
+        leftEye.pitch = headPitch * pi/180;
+        leftEye.yaw = headYaw * pi/180;
+        rightEye.pitch = headPitch * pi/180;
+        rightEye.yaw = headYaw * pi/180;
+
+        //random eye rotation
+        leftEye.roll = MathHelper.sin(animationProgress * speed * 0.05F) * degree * 0.1F;
+        leftEye.pitch += MathHelper.cos(animationProgress * speed * 0.025F) * degree * 0.2F;
+        rightEye.roll = MathHelper.sin(animationProgress * speed * 0.05F + pi) * degree * 0.1F;
+        rightEye.pitch += MathHelper.cos(animationProgress * speed * 0.025F + pi) * degree * 0.2F;
+
+        //retreating in its shell
+        if (entity.isScared()) {
+            this.rightLeg.visible = false;
+            this.leftLeg.visible = false;
+            this.root.pivotY = 8.0F;
+            this.shell.roll = 0.0F;
+        } else {
+            shell.roll = MathHelper.sin(limbAngle * speed * 0.6F) * degree * 0.25F * limbDistance;
+            this.rightLeg.visible = true;
+            this.leftLeg.visible = true;
+            this.root.pivotY = 0.0F;
+        }
+
+        //walking
+        body.pivotY = cos(limbAngle * speed * 0.6F + pi/2) * 1.4F * limbDistance + 11.0F;
+        rightLeg.pitch = cos(limbAngle * speed * 0.6F) * 1.4F * limbDistance;
+        leftLeg.pitch = cos(limbAngle * speed * 0.6F + pi) * 1.4F * limbDistance;
     }
 }
--- src/main/java/com/ninni/species/entity/LimpetEntity.java
+++ src/main/java/com/ninni/species/entity/LimpetEntity.java
@@ -1,26 +1,113 @@
 package com.ninni.species.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public class LimpetEntity extends AnimalEntity {
+    private static final TrackedData<Integer> SCARED_TICKS = DataTracker.registerData(LimpetEntity.class, TrackedDataHandlerRegistry.INTEGER);
+
     protected LimpetEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(5, new EscapeDangerGoal(this, 1.5));
+        this.goalSelector.add(6, new WanderAroundFarGoal(this, 1));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 6));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+    }
+
     public static DefaultAttributeContainer.Builder createLimpetAttributes() {
-        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 8.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25);
+        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 8.0).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25);
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(SCARED_TICKS, 0);
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putInt("ScaredTicks", this.getScaredTicks());
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.setScaredTicks(nbt.getInt("ScaredTicks"));
+    }
+
+    public int getScaredTicks() {
+        return this.dataTracker.get(SCARED_TICKS);
+    }
+    public void setScaredTicks(int scaredTicks) {
+        this.dataTracker.set(SCARED_TICKS, scaredTicks);
+    }
+    public boolean isScared() {
+        return this.getScaredTicks() > 0;
+    }
+
+    @Override
+    public void tickMovement() {
+        super.tickMovement();
+        this.world.getEntitiesByClass(PlayerEntity.class, this.getBoundingBox().expand(2D), this::isValidEntity).forEach(player -> this.setScaredTicks(100));
+        if (this.getScaredTicks() > 0) {
+            this.getNavigation().stop();
+            this.setScaredTicks(this.getScaredTicks() - 1);
+        }
+    }
+
+    @Override
+    public boolean isPushable() {
+        return !this.isScared();
+    }
+
+    private boolean isValidEntity(PlayerEntity entity) {
+        return !entity.isSpectator() && entity.isAlive() && !entity.getAbilities().creativeMode && !entity.isSneaking();
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (source.getAttacker() instanceof LivingEntity && amount < 12 && !world.isClient()) {
+            playSound(SoundEvents.ITEM_SHIELD_BLOCK, 1, 1);
+            this.setScaredTicks(300);
+            return false;
+        }
+        return super.damage(source, amount);
+    }
+
+    @Override
+    public void travel(Vec3d movementInput) {
+        if (this.isScared()) {
+            this.setVelocity(this.getVelocity().multiply(0, 1, 0));
+            movementInput = movementInput.multiply(0, 1, 0);
+        }
+        super.travel(movementInput);
     }
 
     @Nullable
