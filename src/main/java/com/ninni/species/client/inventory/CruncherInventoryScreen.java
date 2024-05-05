7d650f9b84a8d615c9302d582c840ed3be154ae1
0rc1nus <mitchellctlui@gmail.com>
2024-05-05 14:16:04 +0800

    Cycling Slot

--- src/main/java/com/ninni/species/client/inventory/CruncherInventoryScreen.java
+++ src/main/java/com/ninni/species/client/inventory/CruncherInventoryScreen.java
@@ -1,11 +1,14 @@
 package com.ninni.species.client.inventory;
 
+import com.google.common.collect.Lists;
 import com.ninni.species.Species;
 import com.ninni.species.entity.Cruncher;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.Util;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.CyclingSlotBackground;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -13,12 +16,19 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Phantom;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.SmithingTemplateItem;
+
+import java.util.List;
+import java.util.function.Function;
 
 import static net.minecraft.client.gui.screens.inventory.InventoryScreen.renderEntityInInventoryFollowsMouse;
 
 @Environment(EnvType.CLIENT)
 public class CruncherInventoryScreen extends AbstractContainerScreen<CruncherInventoryMenu> {
-    private static final ResourceLocation HAMSTER_INVENTORY_LOCATION = new ResourceLocation(Species.MOD_ID, "textures/gui/container/cruncher.png");
+    private static final ResourceLocation CRUNCHER_INVENTORY_LOCATION = new ResourceLocation(Species.MOD_ID, "textures/gui/container/cruncher.png");
+    private static final Function<String, ResourceLocation> FUNCTION = s -> new ResourceLocation(Species.MOD_ID, "item/empty_slot_" + s);
+    private static final List<ResourceLocation> INPUT_LIST = List.of(FUNCTION.apply("rotten_flesh"), FUNCTION.apply("bone"));
+    private final CyclingSlotBackground inputIcon = new CyclingSlotBackground(0);
     private float xMouse;
     private float yMouse;
     private Cruncher cruncher;
@@ -29,19 +39,25 @@ public class CruncherInventoryScreen extends AbstractContainerScreen<CruncherInv
     }
 
     @Override
+    protected void containerTick() {
+        super.containerTick();
+        this.inputIcon.tick(INPUT_LIST);
+    }
+
+    @Override
     protected void renderBg(GuiGraphics poseStack, float f, int i, int j) {
         this.renderBackground(poseStack);
-        int imgX = (this.width - this.imageWidth) / 2;
-        int imgY = (this.height - this.imageHeight) / 2;
-        poseStack.blit(HAMSTER_INVENTORY_LOCATION, imgX, imgY, 0, 0, this.imageWidth, this.imageHeight);
 
+        poseStack.blit(CRUNCHER_INVENTORY_LOCATION, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+
+        this.inputIcon.render(this.menu, poseStack, f, this.leftPos, this.topPos);
         if (this.cruncher.getPelletData() == null) return;
 
         Entity entity = this.cruncher.getPelletData().entityType().create(cruncher.level());
 
         if (!(entity instanceof LivingEntity livingEntity)) return;
 
-        renderEntityInInventoryFollowsMouse(poseStack, imgX + 118, imgY + 64, 20, (float)(imgX + 118) - this.xMouse, (float)(imgY + 66 - 40) - this.yMouse, livingEntity);
+        renderEntityInInventoryFollowsMouse(poseStack, this.leftPos + 118, this.topPos + 64, 20, (float)(this.leftPos + 118) - this.xMouse, (float)(this.topPos + 66 - 40) - this.yMouse, livingEntity);
     }
 
     public void render(GuiGraphics guiGraphics, int i, int j, float f) {
